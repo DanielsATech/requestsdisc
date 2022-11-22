86f9ea8c1b9add71d97f88773ced8ffbb03284b1
Daniels Auzins <73557869+DanielsATech@users.noreply.github.com>
2022-11-22 04:50:07 +0000

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,57 @@
+# Libs
+import configparser
+import requests
+import json
+
+
+
+
+
+
+
+# Entrypoint Method
+def Main(channelid):
+    class BotHandler():
+        config: configparser.RawConfigParser = configparser.RawConfigParser() #Initialises our config parser.
+        config.read("config.properties") #Looks for config.properties in source & reads it.
+        token: str = config.get("Settings", "TOKEN") #Retrieves token from [Settings]
+        url: str = f"https://discord.com/api/v9/channels/{channelid}/messages" #Channel ID parameter is passed
+
+        
+
+        header = {
+            'authorization': f'{token}' #Auth token
+        }
+
+        reqretrieve = requests.get(url, headers=header)
+        r = json.loads(reqretrieve.text)
+
+        for value in r:
+            print(value)
+            payload = {
+            'content': f'[PAYLOAD] {value} ' #Payload to post to channel
+            }
+            reqpost = requests.post(url, data=payload, headers=header)
+
+       
+
+    
+        
+
+        
+
+    bot = BotHandler()
+        
+                
+        
+
+
+       
+    
+
+
+
+if __name__ == "__main__":
+    Main(1044448061461635102)
+
+        
